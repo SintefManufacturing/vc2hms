c0f755f357be16e943cfda3a691397d89a584f36
Olivier <>
2013-02-12 12:59:39 +0100

    comp signal work with vc2012sp2

--- Listener.cs
+++ Listener.cs
@@ -44,82 +44,77 @@ namespace vc2ice
         }
 
 
-        #region IvcEventPropertyListener Members
         public void notifySetupChanged(ref IvcProperty Property)
         {
         }
+        /*
+        private IvcComponent getComponent(ref IvcProperty Property, object Value)
+        {
+            if (Value == null || Property.ExtendedValue == null)
+            {
+                Console.WriteLine("Component signal does not contain extended value this is not normal !!!!!!!!!!!!");
+                return null;
+            }
+            else
+            {
+                IvcComponent comp = (IvcComponent)Property.ExtendedValue;
+                if (comp.RootNode == null)
+                {
+                    Console.WriteLine("Root node of comp is null!!!!!!!!!!!!");
+                    //string name = comp.getProperty("Name");
+                    //Console.WriteLine("name");
+                    return null;
+                }
+                else
+                {
+                    return comp;
+                }
+            }
 
+        }
+        */
         public void notifyValueChanged(ref IvcProperty Property, object Value)
         {
-
-
             if (m_Type == "ComponentSignal")
             {
-                if (Value != null)
+                Console.WriteLine(getID() + " got Component Signal for comp " + Value + " of type: " + Value.GetType());
+                IvcComponent comp = (IvcComponent)Property.ExtendedValue;
+                //IvcComponent comp = getComponent(ref Property, Value);
+                if (comp != null)
                 {
-                    Console.WriteLine(getID() + " got Component Signal for comp " + Value +" of type: " + Value.GetType());
-
-                    if (Property.ExtendedValue != null)
+                    Console.WriteLine("Find if holon exist");
+                    //find if holon exist for component
+                    string name = comp.getProperty("Name");
+                    string dname = name + comp.getProperty("SessionID"); //name for dynamic components
+                    Ice.ObjectPrx prx = IceApp.getHolon(dname);
+                    if (prx == null)
                     {
-                        IvcComponent comp = (IvcComponent)Property.ExtendedValue;
-                        Console.WriteLine(getID() + " Extended type: " + comp.GetType());
-                        //Console.WriteLine( comp.getProperty("Container::Location"));
-                        string[] list = new string[comp.PropertyCount];
-                        for (int i = 0; i < comp.PropertyCount; i++)
-                        {
-                            string n = comp.getPropertyName(i);
-                            Console.WriteLine("prop: " + n  );
-                            Console.WriteLine("val: " + comp.getProperty(n));
-                        }
-
-                        if (comp.RootNode == null)
-                        {
-                            Console.WriteLine("Root node of comp is null!!!!!!!!!!!!");
-                            //string name = comp.getProperty("Name");
-                            //Console.WriteLine("name");
-                        } else
-                        {
-                            Console.WriteLine("Find if holon exist");
-                            //find if holon exist for component
-                            string name = comp.getProperty("Name");
-                            string dname = name + comp.getProperty("SessionID"); //name for dynamic components
-                            Ice.ObjectPrx prx = IceApp.getHolon(dname);
-                            if (prx == null)
-                            {
-                                Console.WriteLine("was not a dynamic comp");
-                                prx = IceApp.getHolon(name); //FIXME: we might return wonrg object if it is instanciated
-                                dname = name;
-                            }
-                            if (prx != null)
-                            {
-                                Console.WriteLine("Sending event and msg");
-                                //m_pub.newComponentSignal(m_Signal.getProperty("Name"), hms.ComponentPrxHelper.uncheckedCast(prx));
-                                //Now send a message
-                                Helpers.printMatrix("comp pose: ", comp.RootNode.getProperty("WorldPositionMatrix"));
-                                hms.Message msg = new hms.Message();
-                                msg.arguments = new System.Collections.Generic.Dictionary<string, string>();
-                                msg.arguments.Add("EmitorName", m_ComponentName);
-                                msg.arguments.Add("ComponentName",dname);
-                                msg.arguments.Add("SignalName", m_Type);
-                                msg.arguments.Add("SignalType", m_Type);
-                                msg.arguments.Add("WorldPositionMatrix", String.Join(",", comp.RootNode.getProperty("WorldPositionMatrix")));
-                                m_pub.putMessage(msg);
-                            }
-                        }
-
-
-                        //Helpers.printMatrix("comp pose: ", comp.RootNode.RootFeature.getProperty("WorldPositionMatrix"));
+                        prx = IceApp.getHolon(name); //FIXME: we might return wonrg object if it is instanciated
+                        dname = name;
                     }
+                    if (prx != null)
+                    {
+                        Console.WriteLine("Sending event and msg");
+                        //m_pub.newComponentSignal(m_Signal.getProperty("Name"), hms.ComponentPrxHelper.uncheckedCast(prx));
+                        //Now send a message
+                        Helpers.printMatrix("comp pose: ", comp.RootNode.getProperty("WorldPositionMatrix"));
+                        hms.Message msg = new hms.Message();
+                        msg.arguments = new System.Collections.Generic.Dictionary<string, string>();
+                        msg.arguments.Add("EmitorName", m_ComponentName);
+                        msg.arguments.Add("ComponentName", dname);
+                        msg.arguments.Add("SignalName", m_Type);
+                        msg.arguments.Add("SignalType", m_Type);
+                        msg.arguments.Add("WorldPositionMatrix", String.Join(",", comp.RootNode.getProperty("WorldPositionMatrix")));
+                        m_pub.putMessage(msg);
+                    }
+
                 }
-                //IvcComponent comp = (IvcComponent)Value;
-                //Helpers.printMatrix("comp pose: ",comp.RootNode.RootFeature.getProperty( "PositionMatrix") );
             }
             else
             {
                 //first resend signal to Ice
                 //m_pub.newBooleanSignal(m_Signal.getProperty("Name"), Property.ExtendedValue);
                 //Now send a message
-
                 Console.WriteLine(getID() + " got Boolean Signal " + (string)m_Signal.getProperty("Name") + "  " + Value.ToString() + " re-sending it to icehms");
                 hms.Message msg = new hms.Message();
                 msg.arguments = new System.Collections.Generic.Dictionary<string, string>();
@@ -131,6 +126,17 @@ namespace vc2ice
             }
 
         }
-        #endregion
+
+
+        private void print_properties(IvcComponent comp)
+        {
+            string[] list = new string[comp.PropertyCount];
+            for (int i = 0; i < comp.PropertyCount; i++)
+            {
+                string n = comp.getPropertyName(i);
+                Console.WriteLine("prop: " + n);
+                Console.WriteLine("val: " + comp.getProperty(n));
+            }
+        }
     }
 }
