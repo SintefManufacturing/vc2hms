f5695cef5038f1574e8b68d433b82b2ee1a19550
Olivier R-D <dummy@dummy.com>
2013-02-26 11:08:35 +0100

    picking demo works with startstop signal

--- demo/idemo.py
+++ demo/idemo.py
@@ -17,25 +17,43 @@ if __name__ == "__main__":
     mgr.init()
     alive = []
     try:
-        print("Holons are: ", mgr.findHolons())
+        print("Holons are: ", mgr.find_holons())
         try:
-            ida = mgr.getHolon("Ida")
+            ida = mgr.get_holon("Ida")
             ida = ida.ice_timeout(20000)
             alive.append(ida)
-        except Exception, ex:
-            print("Could not create ur", ex)
+        except Exception as ex:
+            pass
+        else:
+            print("found ida")
         try:
-            ur = mgr.getHolon("UR5")
+            ur = mgr.get_holon("UR5")
             ur = ur.ice_timeout(20000)
             alive.append(ur)
-        except Exception, ex:
-            print("Could not create ur", ex)
+        except Exception as ex:
+            pass
+        else:
+            print("found ur")
         try:
-            ur10 = mgr.getHolon("UR10")
+            ur10 = mgr.get_holon("UR10")
             ur10 = ur10.ice_timeout(20000)
             alive.append(ur10)
-        except Exception, ex:
-            print("Could not create ur10", ex)
+        except Exception as ex:
+            pass
+        else:
+            print("found ur10")
+        try:
+            conv = mgr.get_holon("Sensor Conveyor")
+            alive.append(conv)
+        except Exception as ex:
+            pass
+        else:
+            print("found conv")
+
+        msg = icehms.Message()
+        msg.arguments["SignalType"] = "BooleanSignal"
+        msg.arguments["SignalValue"] = "False"
+        msg.arguments["SignalName"] = "StartStop"
 
         from IPython.frontend.terminal.embed import InteractiveShellEmbed
         ipshell = InteractiveShellEmbed( banner1="\nStarting IPython shell, available objects are:\n " + str(alive) + "\n")
--- demo/picking_demo.py
+++ demo/picking_demo.py
@@ -4,7 +4,7 @@ from struct import unpack
 import logging
 import time
 
-from icehms import Holon, startHolonStandalone, hms
+from icehms import Holon, run_holon, hms, Message
 
 #class Client(Holon, hms.GenericEventInterface):
 class CellCtrl(Holon):
@@ -13,37 +13,55 @@ class CellCtrl(Holon):
 
     def run(self):
         #self._subscribeTopic(self._tn)
-        self._subscribeEvent("Sensor Conveyor::SensorSignal")
-        ur = self._icemgr.getHolon("UR10")
+        self._subscribe_topic("Sensor Conveyor::SensorSignal")
+
+
+        stopmsg = Message()
+        stopmsg.arguments["SignalType"] = "BooleanSignal"
+        stopmsg.arguments["SignalValue"] = "False"
+        stopmsg.arguments["SignalName"] = "StartStop"
+
+
+        startmsg = Message()
+        startmsg.arguments["SignalType"] = "BooleanSignal"
+        startmsg.arguments["SignalValue"] = "True"
+        startmsg.arguments["SignalName"] = "StartStop"
+
+        conv = self._icemgr.get_holon("Sensor Conveyor")
+        ur = self._icemgr.get_holon("UR10")
+        init = [0.0, 0.0, 1.5707936732062238, 0, -1.57, 0.0]
+        if False:
+            ur = self._icemgr.get_holon("KR 16-2")
+            init = [0.0, -1.570795, 1.570795, 0.0, 1.57, 0.0]
         ur = ur.ice_timeout(100000)
-        ur.setCSYS(hms.CSYS.World)
-        #ur.movel((1.153155317981248, -0.03674964688098066, 1.221240297626111, 4.712485335623291e-06, -9.668942806603778e-06, 0.17298867413255042), 0.1, 0.01)
-        init = [1.14, 0, 2.06, 6.720322348499538, 4.905581723624449, -1.6546270059573371]
+        ur.set_csys(hms.CSYS.World)
         print("Moving to init pose")
         ur.movej(init, 100, 30)
         while not self._stop:
             time.sleep(0.1)
             if len(self.mailbox) > 0:
                 msg = self.mailbox.pop()
+                conv.put_message(stopmsg)
                 pose  = msg.arguments["WorldPositionMatrix"]
                 print("We have a part at", pose)
                 print("We are at: ", ur.getl())
                 pose = [float(i) for i in pose.split(",")]
                 pose = pose[:3]
-                pose[2] += 0.1
+                pose[2] += 0.2
                 print("Moving to: ", pose)
-                ur.translate(pose, 0.1, 0.1)
+                ur.translate(pose, 0.8, 0.8)
                 ur.grasp()
                 pose[2] += 0.2
                 pose[1] -= 1.0
+                conv.put_message(startmsg)
                 print("start placing at: ", pose)
-                ur.translate(pose, 0.1, 0.1)
+                ur.translate(pose, 0.8, 0.8)
                 pose[2] -= 0.2
-                ur.translate(pose, 0.1, 0.1)
+                ur.translate(pose, 0.8, 0.8)
                 print("release")
                 ur.release()
                 print("Moving to init pose")
-                ur.movej(init, 100, 30)
+                ur.movej(init, 0.01, 1)
 
 
 
@@ -51,4 +69,4 @@ class CellCtrl(Holon):
 
 if __name__ == "__main__":
     s = CellCtrl()
-    startHolonStandalone(s)
+    run_holon(s)
