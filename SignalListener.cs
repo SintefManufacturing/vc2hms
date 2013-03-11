b2f585e5e438a02d5b381e8b98278f146cb49458
Olivier <Olivier@WindowsPC>
2013-03-11 14:44:11 +0100

    add copyright line

--- SignalListener.cs
+++ SignalListener.cs
@@ -1,4 +1,6 @@
 ﻿/*
+     Copyright 2013 Olivier Roulet-Dubonnet
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
